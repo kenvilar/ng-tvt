7a8f76ce82fbe2eef5f367b737af6bc7dbaa0a2c
Ken Vilar <kenvilar@gmail.com>
2018-10-07 00:13:57 +0800

    Search Feature UI

--- sections/search/search.ctrl.js
+++ sections/search/search.ctrl.js
@@ -1,19 +1,29 @@
-angular.
-		module('app.core')
-		.controller('SearchController', function(ShowService) {
-				'ngInject';
+'use strict';
 
+angular
+		.module('app.core')
+		.controller('SearchController', function(ShowService, $timeout) {
 				var vm = this;
 
+				init();
+
+				function init() {
+						vm.results = false;
+						vm.searching = false;
+				}
+
 				vm.query = (query) => {
-						ShowService.search(query).
-								then((res) => {
-										console.log(res);
+						vm.searching = true;
+
+						ShowService.search(query)
+								.then((res) => {
+										vm.results = res;
+										$timeout(() => {
+												vm.searching = false;
+										}, 500);
 								}).
 								catch((err) => {
 										console.log(err);
 								});
 				};
-
-				vm.query("Game of Thrones");
 		});
